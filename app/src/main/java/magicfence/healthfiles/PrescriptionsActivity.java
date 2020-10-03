c39c74dce26d7dc694d4c92095f47ebe5737368a
codenavneet <codenav007@gmail.com>
2020-10-03 22:51:27 +0530

    dashboard thumbnails

---
+++ app/src/main/java/magicfence/healthfiles/PrescriptionsActivity.java
@@ -0,0 +1,14 @@
+package magicfence.healthfiles;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PrescriptionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prescriptions);
+    }
+}
